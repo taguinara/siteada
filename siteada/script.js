02a9c68396888dacc628c5a4aa1ba2ac724391e1
Tainara Almeida <68087649+taguinara@users.noreply.github.com>
2021-01-09 08:05:41 -0300

    Add files via upload

---
+++ siteada/script.js
@@ -0,0 +1,10 @@
+document.getElementById("botao").addEventListener("click", ValidaFormulario)
+
+function ValidaFormulario(){
+  if (document.getElementById("nome").value != "" && document.getElementById  ("email").value !="" &&    document.getElementById("telefone").value != ""){
+     alert("Prontinho! Você receberá as novidades por email")
+ }else{
+     alert("Por favor, preencha os campos nome, email e telefone!")
+ }  
+ 
+}
